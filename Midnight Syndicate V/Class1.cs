82bc18e543f92d6b2987ca3a99a7aa57b2e62b0a
Admin <Admin@DESKTOP-EHT5SO9>
2026-02-24 14:50:20 +0530

    Add project files.

---
+++ Midnight Syndicate V/Class1.cs
@@ -0,0 +1,31 @@
+﻿using GTA;
+using GTA.UI;
+using GTA.Math;
+using NativeUI;
+using GTA.Native;
+using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+public class MidnightSyndicateV : Script
+{
+    public MidnightSyndicateV()
+    {
+        Tick += OnTick;
+        KeyUp += OnKeyUp;
+        Aborted += OnAbort;
+        Interval = 20;
+    }
+
+    private void OnTick(object sender, EventArgs e)
+    {
+    }
+
+    private void OnKeyUp(object sender, KeyEventArgs e)
+    {
+    }
+
+    private void OnAbort(object sender, EventArgs e)
+    {
+    }
+}
